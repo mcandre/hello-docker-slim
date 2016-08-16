0ee2fdbcbb6baca409b5f8d4c9d054130131fdab
Andrew Pennebaker <apennebaker@spredfast.com>
2016-08-16 09:07:11 -0500

    update editorconfig

--- src/hello.asm
+++ src/hello.asm
@@ -9,16 +9,16 @@ section .text
 global _start
 
 _start:
-	;;  write
-	mov rsi, dword msg
-	mov rdx, 13
-	push 0x1
-	pop rax
-	mov rdi, rax
-	syscall
+  ;;  write
+  mov rsi, dword msg
+  mov rdx, 13
+  push 0x1
+  pop rax
+  mov rdi, rax
+  syscall
 
-	;; exit
-	xor rdi, rdi
-	push 0x3c
-	pop rax
-	syscall
+  ;; exit
+  xor rdi, rdi
+  push 0x3c
+  pop rax
+  syscall
